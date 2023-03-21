9ed0b1da32a3c67605ae50fb32bd752fc1e6b6b3
Mehrdad <m.dolatabadi@emofid.com>
2023-03-21 07:56:46 +0330

    Add order to Queries

--- Application/Options/Queries/GetQuestionOptions/GetQuestionOptions.cs
+++ Application/Options/Queries/GetQuestionOptions/GetQuestionOptions.cs
@@ -3,6 +3,9 @@ using Microsoft.EntityFrameworkCore;
 using Tournament.Application.Common.Interfaces;
 using Tournament.Application.Questions.Queries.GetContestQuestions;
 using Tournament.Application.Common.Security;
+using AutoMapper.QueryableExtensions;
+using Tournament.Application.Contests.Queries.GetContests;
+using AutoMapper;
 
 namespace Tournament.Application.Options.Queries.GetQuestionOptions;
 
@@ -19,24 +22,21 @@ public record GetQuestionOptionsQuery : IRequest<List<OptionDto>>
 public class GetQuestionOptionsQueryHandler : IRequestHandler<GetQuestionOptionsQuery, List<OptionDto?>>
 {
 	private readonly IApplicationDbContext _context;
+	private readonly IMapper _mapper;
+
 	// private readonly IMapper _mapper;
-	public GetQuestionOptionsQueryHandler(IApplicationDbContext context/*, IMapper mapper*/)
+	public GetQuestionOptionsQueryHandler(IApplicationDbContext context, IMapper mapper)
 	{
 		_context = context;
-		// _mapper = mapper;
+		_mapper = mapper;
 	}
 
 	public async Task<List<OptionDto>> Handle(GetQuestionOptionsQuery request, CancellationToken cancellationToken)
 	{
 		return await  _context.Options
 			.Where(x => x.QuestionId == request.QuestionId)
-			.Select(x=> new OptionDto
-					{
-					Id = x.Id,
-					Text = x.Text,
-					Title = x.Title,
-					IsAnswer = x.IsAnswer
-					}).ToListAsync();
+            .ProjectTo<OptionDto>(_mapper.ConfigurationProvider)
+           .ToListAsync();
 	}
 }
 
