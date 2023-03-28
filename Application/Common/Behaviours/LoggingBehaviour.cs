92326b128173721f3b6596223bba8b5777cb3be1
Mehrdad <m.dolatabadi@emofid.com>
2023-03-28 10:16:43 +0430

    Log Username

--- Application/Common/Behaviours/LoggingBehaviour.cs
+++ Application/Common/Behaviours/LoggingBehaviour.cs
@@ -1,21 +1,25 @@
 using MediatR.Pipeline;
 using Microsoft.Extensions.Logging;
+using Tournament.Application.Common.Interfaces;
+using Tournament.Application.Common.Security;
 
 namespace Tournament.Application.Common.Behaviours;
 
 public class LoggingBehaviour<TRequest> : IRequestPreProcessor<TRequest> where TRequest : notnull
 {
     private readonly ILogger _logger;
+    private readonly ICurrentUserService _currentUserService;
 
-    public LoggingBehaviour(ILogger logger )
+    public LoggingBehaviour(ILogger logger,ICurrentUserService currentUserService)
    { 
         _logger = logger;
+        _currentUserService = currentUserService;
     }
 
     public async Task Process(TRequest request, CancellationToken cancellationToken)
     {
         var requestName = typeof(TRequest).Name;
-        string userName = string.Empty;
+        string userName = _currentUserService.UserName ?? string.Empty;
 
 
         _logger.LogInformation("Tournament Request: {Name} {@UserId} {@UserName} {@Request}",
