bfb03bd10ecac5d688a5a9652452cce66548022f
Mehrdad <m.dolatabadi@emofid.com>
2023-04-10 23:48:32 +0430

    Fix Update2

--- Application/Contests/Commands/UpdateContest/UpdateContestCommand.cs
+++ Application/Contests/Commands/UpdateContest/UpdateContestCommand.cs
@@ -6,6 +6,7 @@ using static System.Net.Mime.MediaTypeNames;
 using Microsoft.EntityFrameworkCore;
 using FluentValidation.Results;
 using Tournament.Domain.Entities;
+using System;
 
 namespace Tournament.Application.Contests.Commands.UpdateContest;
 
@@ -27,18 +28,18 @@ public sealed record UpdateContestCommand : IRequest<int>
 	public int ParticipationCapacity { get; init; }
 	public List<QuestionCreateDto> Questions { get; init; }
 }
-public record QuestionCreateDto : IEquatable<QuestionCreateDto>
+public sealed record QuestionCreateDto : IEquatable<QuestionCreateDto>
 {
     public string? Title { get; init; }
     public int Order { get; init; }
-    public List<OptionUpdateDto> options { get; init; }
-    public bool Equals(OptionUpdateDto? other)
+    public IEnumerable<OptionUpdateDto> options { get; init; }
+    public bool Equals(QuestionCreateDto? other)
     {
         if (other == null)
             return false;
 
         bool title_eq = (string.IsNullOrEmpty(Title) && string.IsNullOrEmpty(other.Title)) || (Title.Equals(other?.Title));
-		bool options_eq = options.OrderBy(x=>x).SequenceEqual<OptionUpdateDto>(options.OrderBy(x=>x));
+		bool options_eq =  options.OrderBy(x=>x.Title).SequenceEqual(other.options.OrderBy(x=>x.Title));
         return title_eq && options_eq;
     }
 }
@@ -52,8 +53,8 @@ public sealed record OptionUpdateDto : IEquatable<OptionUpdateDto>
         if (other == null)
             return false;
 
-        bool title_eq = (string.IsNullOrEmpty(Title) && string.IsNullOrEmpty(other.Title)) || (Title.Equals(other?.Title));
-		bool text_eq = (string.IsNullOrEmpty(Text) && string.IsNullOrEmpty(other.Text)) || (Text.Equals(other.Text));
+        bool title_eq = (string.IsNullOrEmpty(Title) && string.IsNullOrEmpty(other?.Title)) || (Title.Equals(other?.Title));
+		bool text_eq = (string.IsNullOrEmpty(Text) && string.IsNullOrEmpty(other?.Text)) || (Text.Equals(other?.Text));
 		return title_eq && text_eq;
     }
 }
@@ -77,7 +78,7 @@ public class UpdateContestCommandHandler : IRequestHandler<UpdateContestCommand,
 		}
 
 		//Compare questions and options
-		var QOs_Changed =! oldEntity.Questions.Select(x=>new QuestionCreateDto { Title=x.Title, options=x.Options.Select(y=>new OptionUpdateDto { Title=y.Title, Text=y.Text }).ToList() })
+		var QOs_Changed =! oldEntity.Questions.Select(x=>new QuestionCreateDto { Title=x.Title, options=x.Options.Select(y=>new OptionUpdateDto { Title=y.Title, Text=y.Text }).OrderBy(t=>t.Title) })
 			.OrderBy(x => x).SequenceEqual(request.Questions.OrderBy(x => x));
 		//(if has participations throw error "this contest has participations and you can't update the questions or options in this case") remove QOs and append
 		var has_participations = _context.Participations.Any(x => x.ContestId == request.ContestId);
@@ -87,8 +88,10 @@ public class UpdateContestCommandHandler : IRequestHandler<UpdateContestCommand,
             if (has_participations)
             {
                 throw new ValidationException(new List<ValidationFailure> { new ValidationFailure() { ErrorCode = "881", ErrorMessage = "This contest has participations and you can't update the questions or options." } });
-            }
-			oldEntity.Questions = request.Questions.Select(x => new Question { Title = x.Title, Options = x.options.Select(y => new Option { Title = y.Title, Text = y.Text }).ToList() }).ToList();
+			}
+            _context.Options.RemoveRange(oldEntity.Questions.SelectMany(x => x.Options));
+            _context.Questions.RemoveRange(oldEntity.Questions);
+            oldEntity.Questions = request.Questions.Select(x => new Question { Title = x.Title, Options = x.options.Select(y => new Option { Title = y.Title, Text = y.Text }).ToList() }).ToList();
         }
         //else 
         //if QOs not modified update contest
@@ -110,7 +113,7 @@ public class UpdateContestCommandHandler : IRequestHandler<UpdateContestCommand,
 		oldEntity.WinnersCapacity=request.WinnersCapacity;
 		oldEntity.ParticipationCapacity = request.ParticipationCapacity;
 
-		_context.Contests.Add(oldEntity);
+		_context.Contests.Update(oldEntity);
 		await _context.SaveChangesAsync(cancellationToken);
 		return oldEntity.Id;
 	}
