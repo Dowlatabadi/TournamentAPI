e5f2cd93b909b44bb0e2133b4fcf8b6b0cda5635
Mehrdad <m.dolatabadi@emofid.com>
2023-04-11 07:16:50 +0430

    Add Cap Check

--- Application/Participations/Commands/CreateParticipation/CreateParticipationCommandValidator.cs
+++ Application/Participations/Commands/CreateParticipation/CreateParticipationCommandValidator.cs
@@ -1,6 +1,7 @@
 using FluentValidation;
 using Tournament.Application.Common.Interfaces;
 using Microsoft.EntityFrameworkCore;
+using Tournament.Domain.Entities;
 
 namespace Tournament.Application.Participations.Commands.CreateParticipation;
 
@@ -20,14 +21,25 @@ public class CreateParticipationCommandValidator : AbstractValidator<CreateParti
 			.MustAsync(NumberOfAnswersMatch).WithMessage($"Number of answers and Qs mismatch.");
 
 		RuleFor(v=>v)
-			.MustAsync(NotDupQuestion).WithMessage($"Repetition in Answers of a Question.");
+			.MustAsync(HasFreeCapacity).WithMessage($"Participation capacity has no free room.");
+
+        RuleFor(v => v)
+    .MustAsync(NotDupQuestion).WithMessage($"Repetition in Answers of a Question.");
     }
 
 	public async Task<bool> NotParticipatedYet(CreateParticipationCommand req, CancellationToken cancellationToken){
 		return !await _context.Participations.AnyAsync(x=>x.AccountId==req.AccountId && x.ContestId==req.ContestId );
 	}
-
-	public async Task<bool> NumberOfAnswersMatch(CreateParticipationCommand req, CancellationToken cancellationToken){
+    public async Task<bool> HasFreeCapacity(CreateParticipationCommand req, CancellationToken cancellationToken)
+	{
+        var contestCap = (await _context.Contests.Where(x => x.Id == req.ContestId).FirstOrDefaultAsync())?.ParticipationCapacity;
+		if (contestCap != null)
+		{
+        return (await _context.Participations.Where(x =>x.ContestId == req.ContestId).CountAsync())<contestCap;
+		}
+		return false;
+    }
+    public async Task<bool> NumberOfAnswersMatch(CreateParticipationCommand req, CancellationToken cancellationToken){
 		var questions=_context.Questions.Where(x=>x.ContestId==req.ContestId);
 		return questions.Count()==req.OptionIds.Count;
 	}
